88a83b03bc3e657f32416f60f70ee6bc8ef4c703
jawor <jakubjaworski@mailbox.org>
2026-03-19 15:48:53 +0100

    My new bolid

--- someblocks/blocks.h
+++ someblocks/blocks.h
@@ -3,14 +3,14 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     { "",         "sb-mpd",       1,    3  },
     { "",         "sb-date",     10,    10 },
-    { "",         "sb-volume",    1,    9 },
-    { "",         "battery",     30,    11},
-    { "",         "sb-internet",  5,    7},
+    { "",         "sb-battery",  30,    11 },
+    { "",         "sb-volume",    1,    9  },
+    { "",         "sb-weather",3600,    8  },
 
 };
 
 
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
-static unsigned int delimLen = 5;
+static char delim[] = " | ";
+static unsigned int delimLen = 4;
